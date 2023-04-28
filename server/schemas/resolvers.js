ca3a28eb3b05a157a810081b1168848b8dccce01
byunn90 <kayhanturk90@Live.com>
2023-04-29 08:09:36 +1000

    finished typedefs, booksave

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -28,7 +28,37 @@ const resolvers = {
         return user;
       },
       // saveBook
-      saveBook: async (_, { book }, Content) => {},
+      saveBook: async (_, { book }, context) => {
+        // Check if the user is logged in
+        if (!context.user) {
+          throw new Error("You must be logged in to save a book!");
+        }
+
+        // Get the user's ID from the context object
+        const userId = context.user._id;
+
+        // Find the user in the database
+        const user = await User.findById(userId);
+
+        // Create a new Book object with the necessary fields
+        const newBook = new Book({
+          title: book.title,
+          authors: book.authors,
+          description: book.description,
+          bookId: book.bookId,
+          image: book.image,
+          link: book.link,
+        });
+
+        // Add the new book to the user's savedBooks array
+        user.savedBooks.push(newBook);
+
+        // Save the changes to the database
+        await user.save();
+
+        // Return the updated user object
+        return user;
+      },
       removeBook: async (_, { _id }) => {
         const remove = await Book.findByIdAndRemove(_id);
         return remove;
