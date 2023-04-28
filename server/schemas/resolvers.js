2b481d3dbea68d4c68e10e31eb3d4664d80735cb
byunn90 <kayhanturk90@Live.com>
2023-04-29 05:06:32 +1000

    typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,18 @@
+const { Books, User } = require("../models");
+
+const resolvers = {
+  Query: {
+    User: async () => {
+      return User.find({});
+    },
+    User: async (_, { id }) => {
+      return User.findOne({ _id: id });
+    },
+
+    bookTitle: async () => {
+      return Books.find({});
+    },
+  },
+};
+
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,29 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type Profile {
+    _id: ID!
+    name: String!
+  }
+  type Book {
+    _id: ID!
+    authors: [String!]!
+    description: String!
+    title: String!
+    image: String
+    link: String
+  }
+  type Author {
+    _id: ID!
+    name: String!
+  }
+  type User {
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+    bookCount: Int!
+  }
+`;
+
 module.exports = typeDefs;
